a7c80572ce081e1c1d6df020a6c9023e9c024749
MantovaniGabriel <gabriel.0henrique.m@gmail.com>
2022-03-28 14:06:51 -0300

    simples-code

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 My first exercise about datas in python using the naive bayes. 🐍 🐍
 
 # Credits and assignments
-* dataset: [Mendeley](https://data.mendeley.com/datasets/27cndjvfbx/1/files/bedd6e01-9107-4cf7-8d38-42ab966b0f42) 
-* license: CC BY 4.0
+* dataset: [UCI](https://archive.ics.uci.edu/ml/datasets/Adult)
 
 # Code
-A personal program that assesses the risk of giving someone a loan.
+Program made in a course on python, the program assesses the risk of giving someone a loan using the Naive Bayes algorithm.
--- main.py
+++ main.py
@@ -1,32 +1,55 @@
 import numpy as np
-import plotly.express as pl #lib for graphics.
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns #lib for graphics interface.
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score, classification_report
 import pickle
 
 #########Find file dataset.csv###################
-dataset_credit = pd.read_csv('Datasets/dataset.csv')
+dataset_credit = pd.read_csv('Datasets/credit_risk_dataset.csv')
 
 ##############Data processing##############
 #Null values
 #Removendo idades inválidas
 dataset_credit2 = dataset_credit.drop(dataset_credit[dataset_credit['age'] <= 17].index)
 
-#Removendo colunas não uteis para o uso.
-dataset_credit2 = dataset_credit2.drop('special disease', axis = 1)
-dataset_credit2 = dataset_credit2.drop('religion', axis = 1)
-dataset_credit2 = dataset_credit2.drop('break in love', axis = 1)
-dataset_credit2 = dataset_credit2.drop('smoke', axis = 1)
-
 #Verificando dados nulos.
-print(dataset_credit2.isnull())
+print(dataset_credit2.loc[pd.isnull(dataset_credit2['loan'])]) #printa o valor NaN
+dataset_credit2['loan'].fillna(dataset_credit2['loan'].mean(), inplace = True) #Corrige o valor NaN
+print(dataset_credit2.loc[pd.isnull(dataset_credit2['loan'])]) #printa o valor NaN
 
 #Previsores e Divisores.
+x_dtCredit = dataset_credit2.iloc[:, 1:4].values
+y_dtCredit = dataset_credit2.iloc[:, 4].values
 
-#print(dataset_credit2)
+print(f'\nX_credit = {x_dtCredit}\n\nY_credit = {y_dtCredit}\n\n')
+
+#padronizando os dados.
+scale_credit = StandardScaler()
+x_dtCredit = scale_credit.fit_transform(x_dtCredit)
+
+#base de treinamento.
+x_dtCredit_treinamento, x_dtCredit_teste, y_dtCredit_treinamento, y_dtCredit_teste = train_test_split(x_dtCredit, y_dtCredit, test_size = 0.08, random_state = 0)
+
+#salvando dados em um arquivo.
+with open('Datasets/credit.pkl', mode = 'wb') as f:
+    pickle.dump([x_dtCredit_treinamento, y_dtCredit_treinamento, x_dtCredit_teste, y_dtCredit_teste], f)
+
+####Naive Bayes#####
+#abrindo o arquivo com as informações salvas anteriormente
+with open('Datasets/credit.pkl', 'rb') as f:
+    x_dtCredit_treinamento, y_dtCredit_treinamento, x_dtCredit_teste, y_dtCredit_teste = pickle.load(f)
+
+naive_dtCredit_data = GaussianNB()
+naive_dtCredit_data.fit(x_dtCredit_treinamento, y_dtCredit_treinamento)
+
+prevision_dtCredit_data = naive_dtCredit_data.predict(x_dtCredit_teste)
+
+print(prevision_dtCredit_data)
+print(y_dtCredit_teste)
+print("#--- Precisão ---#")
+print(f'{accuracy_score(y_dtCredit_teste, prevision_dtCredit_data) * 100}%')
